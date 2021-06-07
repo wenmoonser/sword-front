8e079361d8538f270fa905c8b4dfd410d9ab834c
Jason <jasonang0210@gmail.com>
2021-06-07 14:37:19 +0800

    'changes'

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: '',
+            screenName: 'AndromedaDefi',
           }}
           options={{
             height: '300',
